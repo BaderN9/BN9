ed6003aef2e5519331c268adf4047d6a888f8d90
BaderN9 <54602996+BaderN9@users.noreply.github.com>
2019-11-12 18:56:23 -0500

    e17 changes

--- e17/js/main.js
+++ e17/js/main.js
@@ -1,3 +1,17 @@
 $(document).ready(function() {
+
   console.log('THE DOCUMENT IS READY!')
+
+  $('.title').click(function() {
+    $('.n1').toggleClass('font-size');
+    });
+    $('.first').click(function() {
+      $('.Final08').toggleClass('align');
+  });
+    $('.second').click(function() {
+      $('.PSG').toggleClass('align2');
+    });
+    $('.third').click(function() {
+      $('.Final98').toggleClass('align3');
+    });
 });
